d311754e914a7a123956dced283a8ceae239134f
michaeljohnorlando <michael.john.orlando@gmail.com>
2017-04-29 20:41:03 -0400

    need to be able to move with arrow keys now

--- Bacon_Bucket.cc
+++ Bacon_Bucket.cc
@@ -7,7 +7,28 @@
 #include<conio.h>
 #include<cstdlib>
 
-// wont print a function if I use a prototype...?
+// wont print a function if I use a prototype...?...several prototype issues...
+void prepare_cmd_size(){
+  HANDLE wHnd;    // Handle to write to the console.
+  HANDLE rHnd;    // Handle to read from the console.
+  // Set up the handles for reading/writing:
+  wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+  rHnd = GetStdHandle(STD_INPUT_HANDLE);
+  // Change the window title:
+  SetConsoleTitle(TEXT("Bacon Bucket"));
+  // Set up the required window size:
+  _SMALL_RECT console_size;
+  console_size.Top = 0;
+  console_size.Left = 0;
+  console_size.Bottom= 45;
+  console_size.Right = 115;
+  // Change the console window size:
+  SetConsoleWindowInfo(wHnd, TRUE, &console_size);
+  // Create a COORD to hold the buffer size:
+  COORD bufferSize = {30, 50};
+  // Change the internal buffer size:
+  SetConsoleScreenBufferSize(wHnd, bufferSize);
+}
 void GoToXY(int column, int line){
     // Create a COORD structure and fill in its members.
     // This specifies the new position of the cursor that we will set.
@@ -29,51 +50,42 @@ void GoToXY(int column, int line){
         // ...
     }
 }
-void assii_bucket(){std::cout     <<",.--'`````'--.,\n"
-              <<"|'-.,_____,.-'|\n"
-              <<"| -.,_____,.- |\n"
-              <<"|             |\n"
-              <<"|             |\n"
-              <<"|             |\n"
-              <<"`'-.,_____,.-''\n";
-}
+/*################################################
+  # method of moving things with minimal flicker #
+  # an array with a loop that will print in a for#
+  # loop, changeing the vertical...(windows API) #
+  # array sligtly faster then a vector...        #
+  ################################################*/
+  void print_array_at_coordinate(int x, int y,std::string(array)[0]){
+    for(int i=0;i<6;i++){
+      GoToXY(x,i+y);
+      std::cout<<array[i];
+    }
+  }
+// All the ASSII ART arrays
+std::string assii_bucket_array[6]={
+  ",.--'`````'--.,",
+  "|'-.,_____,.-'|",
+  "| -.,_____,.- |",
+  "|             |",
+  "|             |",
+  "`'-.,_____,.-''"
+};
+std::string assii_bacon_array[4]={
+     "   .-'__`-._.'.--.'.__.,",
+     " /--'  '-._.'    '-._./",
+     "/__.--._.--._.'``-.__/",
+     "'._.-'-._.-._.-''-..'"};
 
 
 main(){
-  HANDLE wHnd;    // Handle to write to the console.
-  HANDLE rHnd;    // Handle to read from the console.
-  // Set up the handles for reading/writing:
-  wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
-  rHnd = GetStdHandle(STD_INPUT_HANDLE);
-  // Change the window title:
-  SetConsoleTitle(TEXT("Bacon Bucket"));
-  // Set up the required window size:
-  _SMALL_RECT console_size;
-  console_size.Top = 0;
-  console_size.Left = 0;
-  console_size.Bottom= 45;
-  console_size.Right = 115;
-  // Change the console window size:
-  SetConsoleWindowInfo(wHnd, TRUE, &console_size);
-  // Create a COORD to hold the buffer size:
-  COORD bufferSize = {500, 500};
-  // Change the internal buffer size:
-  SetConsoleScreenBufferSize(wHnd, bufferSize);
-
-
-    here:
-    GoToXY(5,5);
-    std::cout<<"test";
-    for(int delay=1;delay<=300000000;delay++);
-
-    GoToXY(0,5);
-    assii_bucket();
-    for(int delay=1;delay<=300000000;delay++);
-
-goto here;
+  prepare_cmd_size();
+  start:
+  print_array_at_coordinate(30,39,assii_bucket_array);
+  print_array_at_coordinate(30,30,assii_bacon_array);
+  goto start;
 }
-
-std::string thing [3]= {"  o\n"," oo\n","ooo\n"};
-
 // windowed fullscreen
 //ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+//goto here; for recursive things
+//for(int delay=1;delay<=3000000;delay++);     to slow things down ...goog for seeing errors
